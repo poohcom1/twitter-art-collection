5442279bee0ec8f3e8cb2fe0dd1eda7d2c10f52d
poohcom1 <pooh677@gmail.com>
2022-02-23 12:49:13 +0700

    Switched to oauth1.0

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,30 @@
-import NextAuth, { DefaultUser } from 'next-auth';
+import NextAuth from 'next-auth';
 import TwitterProvider from 'next-auth/providers/twitter';
 
+const AUTH_VER = process.env.TWITTER_AUTH_VER ?? "1.0"
+
+// If auth_ver 1.0
+const appId = process.env.TWITTER_API_KEY
+const appSecret = process.env.TWITTER_API_SECRET
+
+// If auth_ver 2.0
 const clientId = process.env.TWITTER_CLIENT_ID
 const clientSecret = process.env.TWITTER_CLIENT_SECRET
 
-if (!clientId || !clientSecret) {
-    throw new Error("Missing TWITTER_CLIENT_ID or TWITTER_CLIENT_SECRET env variables!")
+if (AUTH_VER === "1.0" && (!appId || !appSecret)) {
+    throw new Error("Missing TWITTER_API_KEY or TWITTER_API_SECRET env variables for Twitter OAuth1.0!")
+}
+
+if (AUTH_VER === "2.0" && (!clientId || !clientSecret)) {
+    throw new Error("Missing TWITTER_CLIENT_ID or TWITTER_CLIENT_SECRET env variables for Twitter OAuth2.0!")
 }
 
 export default NextAuth({
     providers: [
         TwitterProvider({
-            clientId,
-            clientSecret,
-            version: "2.0", // opt-in to Twitter OAuth 2.0,
+            clientId: AUTH_VER === "2.0" ? clientId! : appId!,
+            clientSecret: AUTH_VER === "2.0" ? clientSecret! : appSecret!,
+            version: AUTH_VER ?? "1.0", // opt-in to Twitter OAuth 2.0,
         })
     ],
     pages: {
