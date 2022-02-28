27b5121244f1443937e254b40443f40e4c122cb0
poohcom1 <pooh677@gmail.com>
2022-02-28 14:40:43 +0700

    Added preact support

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const configs = {
     images: {
         domains: ['pbs.twimg.com'],
     },
+    webpack: (config, { dev, isServer }) => {
+        // Replace React with Preact only in client production build
+        if (!dev && !isServer) {
+            Object.assign(config.resolve.alias, {
+                react: 'preact/compat',
+                'react-dom/test-utils': 'preact/test-utils',
+                'react-dom': 'preact/compat',
+            });
+        }
+
+        return config;
+    },
 };
 
 module.exports = process.env.ANALYZE === 'true' ? (require('@next/bundle-analyzer')({
