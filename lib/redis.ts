f1bf41bdc5f0826d6028eed3d37ad880200bfae0
poohcom1 <pooh677@gmail.com>
2022-05-13 01:40:59 +0700

    Added redis error handling

--- lib/redis.ts
+++ lib/redis.ts
@@ -8,6 +8,7 @@ export async function getRedis(): Promise<
 > {
   try {
     const redis = createClient({ url: process.env.REDIS_URL });
+    redis.on("error", (err) => console.log("Redis Client Error", err));
     await redis.connect();
     return redis;
   } catch (e) {
