0b1db1c5813468228878b8ea45879d7fc793a86e
poohcom1 <pooh677@gmail.com>
2022-05-26 21:36:42 +0700

    Updated to twitter v2 hometimeline endpoint

--- pages/api/feed-tweets.ts
+++ pages/api/feed-tweets.ts
@@ -1,14 +1,12 @@
-import { getTwitterOAuth, tweetExpansions } from "lib/twitter/twitter";
+import { storeTweetCache, useRedis } from "lib/redis";
+import {
+  createTweetObjects,
+  getTwitterOAuth,
+  TWEET_OPTIONS,
+} from "lib/twitter/twitter";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
-import { ApiResponseError } from "twitter-api-v2";
 
-/**
- * Not working
- * @param req ;
- * @param res
- * @returns
- */
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -16,42 +14,33 @@ export default async function handler(
   const user = await getToken({ req });
 
   if (!user) {
-    return;
+    return res.status(401).end();
   }
 
+  const twitterApi = await getTwitterOAuth(user);
+
+  const pagination_token = (req.query.token as string) ?? undefined;
+
   try {
-    const twitterApi = await getTwitterOAuth(user);
+    const payload = await twitterApi.v2.homeTimeline({
+      ...TWEET_OPTIONS,
+      pagination_token,
+    });
 
-    const max_id = (req.query.token as string) ?? undefined;
+    const token = payload.data.meta?.next_token || undefined;
 
-    const payload = await twitterApi.v1.homeTimeline({ max_id, count: 200 });
+    const tweets = createTweetObjects(payload.data);
 
-    const tweetsV1 = payload.tweets.filter((tweet) =>
-      tweet.entities.media?.find((media) => media.type === "photo")
-    );
-
-    // Replace retweet with original tweet
-    // FIXME Possibly against guidelines; might be better to create a retweet display on the frontend
-    for (const tweet of tweetsV1) {
-      if (tweet.retweeted_status!) {
-        tweet.id_str = tweet.retweeted_status.id_str;
-      }
-    }
-
-    const tweets = await tweetExpansions(tweetsV1.map((t) => t.id_str));
+    await useRedis(storeTweetCache(tweets));
 
     const response: TweetsResponse = {
-      nextToken: tweets.length > 0 ? tweets[tweets.length - 1].id : undefined,
-      tweets: tweets,
+      nextToken: token,
+      tweets,
     };
 
     res.send(response);
   } catch (e) {
-    if (e instanceof ApiResponseError && e.code === 429) {
-      return res.send({ tweets: [] });
-    }
     console.error(e);
-
-    res.status(500).send("Something went wrong :(");
+    res.status(500).send("Server error! Failed to fetch liked tweets!");
   }
 }
